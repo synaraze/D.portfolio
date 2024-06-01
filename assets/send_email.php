d6acb0f6d6d7812034ceca6e8c4f28cee707169f
synaraze <irfandaniealibrahim@gmail.com>
2024-06-01 17:28:23 +0800

    Update send_email.php

--- assets/send_email.php
+++ assets/send_email.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
-    $recipient = "your-email@example.com"; // Replace with your email address
+    $recipient = "irfandaniealibrahim@gmail.com"; // Replace with your email address
     $subject = "New contact from $name";
 
     $email_content = "Name: $name\n";
